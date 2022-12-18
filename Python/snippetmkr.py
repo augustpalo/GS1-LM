aa616027e755250ab520055ef6b938d1a153bb5a
AugustP <augustpaloluoma@gmail.com>
2022-12-18 12:48:07 +0100

    Modified snippet maker, added quote replacement

--- Python/snippetmkr.py
+++ Python/snippetmkr.py
@@ -1,5 +1,17 @@
 import os
 
+# Read in the file
+with open('file', 'r') as i :
+  filedata = i.read()
+
+# Replace the target string
+filedata = filedata.replace('"', '\'')
+
+# Write the file out again
+with open('file', 'w') as i:
+  i.write(filedata)
+
+# Append quotes and comma to every line
 with open ("file") as i:
     with open ("temp", "w") as o:
         for l in i:
